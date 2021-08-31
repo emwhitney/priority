c715257b9e856f0a22892b8ec29ed6328bcce948
Emily Whitney <emilywhitney@emilys-mbp.myfiosgateway.com>
2021-08-31 16:52:25 -0400

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Quiz from './Quiz';
+
+function Home() {
+    return (
+        <div>
+            Welcome to ADHD task tracker!
+            <Quiz/>
+        </div>
+    )
+}
+
+export default Home
